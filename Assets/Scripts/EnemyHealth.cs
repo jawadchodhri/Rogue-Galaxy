c73b0ccf44507577e93854e97bd4ce6070020355
Jawad Chodhri <mj28015@gmail.com>
2026-05-06 19:10:19 +0500

    Health bars

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class EnemyHealth : MonoBehaviour
 {
-    [SerializeField] private float maxHealth = 1;
+    [Header("Health")]
+    [SerializeField] private float maxHealth = 3f;
+
+    [Header("UI")]
+    [SerializeField] private Image healthBar;
 
     private float currentHealth;
     private EnemyWaveMember waveMember;
@@ -14,26 +20,65 @@ public class EnemyHealth : MonoBehaviour
 
     private void OnEnable()
     {
-        ResetHealth();
-    }
-
-    public void ResetHealth()
-    {
         currentHealth = maxHealth;
+        UpdateHealthBar();
     }
 
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
 
-        if (currentHealth <= 0)
+        UpdateHealthBar();
+
+        if (currentHealth <= 0f)
         {
             if (waveMember != null)
-            {
                 waveMember.DieByPlayer();
-            }
             else
                 Destroy(gameObject);
         }
     }
+
+    private void UpdateHealthBar()
+        {
+            if (healthBar == null) return;
+
+            healthBar.fillAmount = currentHealth / maxHealth;
+        }
 }
+
+    // [SerializeField] private float maxHealth = 1;
+
+    // private float currentHealth;
+    // private EnemyWaveMember waveMember;
+
+    // private void Awake()
+    // {
+    //     waveMember = GetComponent<EnemyWaveMember>();
+    // }
+
+    // private void OnEnable()
+    // {
+    //     ResetHealth();
+    // }
+
+    // public void ResetHealth()
+    // {
+    //     currentHealth = maxHealth;
+    // }
+
+    // public void TakeDamage(float damage)
+    // {
+    //     currentHealth -= damage;
+
+    //     if (currentHealth <= 0)
+    //     {
+    //         if (waveMember != null)
+    //         {
+    //             waveMember.DieByPlayer();
+    //         }
+    //         else
+    //             Destroy(gameObject);
+    //     }
+    // }
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [Header("Health")]
+    [SerializeField] private float maxHealth = 100f;
+    [SerializeField] private float collisionDamage = 20f;
+
+    [Header("UI")]
+    [SerializeField] private Image healthFill;
+
+    private float currentHealth;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthUI();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+
+        currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
+
+        UpdateHealthUI();
+
+        if (currentHealth <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private void UpdateHealthUI()
+    {
+        if (healthFill == null) return;
+
+        healthFill.fillAmount = currentHealth / maxHealth;
+    }
+
+    private void Die()
+    {
+        Debug.Log("Player Died");
+
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        EnemyHealth enemy = other.GetComponent<EnemyHealth>();
+
+        if (enemy == null)
+            enemy = other.GetComponentInParent<EnemyHealth>();
+
+        if (enemy == null) return;
+
+        TakeDamage(collisionDamage);
+
+        EnemyWaveMember waveMember = other.GetComponent<EnemyWaveMember>();
+
+        if (waveMember == null)
+            waveMember = other.GetComponentInParent<EnemyWaveMember>();
+
+        if (waveMember != null)
+            waveMember.DieByPlayer();
+        else
+            Destroy(other.gameObject);
+    }
+
+}
