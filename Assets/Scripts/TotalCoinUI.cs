7dea8680cb5e5eea897c9439d7f2e4e069079eef
Jawad Chodhri <152736441+jawadchodhri@users.noreply.github.com>
2026-06-17 22:01:53 +0500

    Coin saving functionality added

--- Assets/Scripts/TotalCoinUI.cs
+++ Assets/Scripts/TotalCoinUI.cs
@@ -1,32 +1,49 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
 public sealed class TotalCoinUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text coinText;
+    [SerializeField] private string prefix = "";
+
+    private Coroutine waitRoutine;
 
     private void OnEnable()
     {
-        if (CoinWallet.Instance == null)
-            return;
+        waitRoutine = StartCoroutine(WaitForWalletRoutine());
+    }
+
+    private void OnDisable()
+    {
+        if (waitRoutine != null)
+        {
+            StopCoroutine(waitRoutine);
+            waitRoutine = null;
+        }
+
+        if (CoinWallet.Instance != null)
+        {
+            CoinWallet.Instance.OnCoinsChanged -= UpdateCoins;
+        }
+    }
+
+    private IEnumerator WaitForWalletRoutine()
+    {
+        while (CoinWallet.Instance == null)
+        {
+            yield return null;
+        }
 
         CoinWallet.Instance.OnCoinsChanged += UpdateCoins;
         UpdateCoins(CoinWallet.Instance.TotalCoins);
     }
 
-    private void OnDisable()
-    {
-        if (CoinWallet.Instance == null)
-            return;
-
-        CoinWallet.Instance.OnCoinsChanged -= UpdateCoins;
-    }
-
     private void UpdateCoins(int coins)
     {
-        if (coinText != null)
-        {
-            coinText.text = coins.ToString();
-        }
+        if (coinText == null)
+            return;
+
+        coinText.text = prefix + coins.ToString();
     }
 }
